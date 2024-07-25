f7a5a706d0305f385b509b1a8655e6f57b41b782
isaque <161765934+canetinha1@users.noreply.github.com>
2024-07-25 16:40:52 -0300

    isaque

--- pingpong.js
+++ pingpong.js
@@ -1,3 +1,8 @@
+//aqui eu vou criar a bolinha
+let xBolinha = 300;
+let yBolinha = 200;
+let tamBolinha = 25;
+
 function setup(){
     //aqui vou criar minha "mesa"
     createCanvas(600,400);
@@ -6,6 +11,12 @@ function setup(){
 function draw(){
     //aqui vou por a cor da "mesa"
     //A cor da mesa é em rgb
-    background(25,25,112); 
+    background(128,128,0); 
+    //chamando a função
+    criaBolinha(xBolinha, yBolinha, tamBolinha);
 }
 
+//função bolinha
+function criaBolinha(xBolinha, yBolinha,tamBolinha){
+    circle (xBolinha,yBolinha,tamBolinha);
+}
