4c041d4045221ec097f19bf057b99928d8410d99
isaque <161765934+canetinha1@users.noreply.github.com>
2024-07-25 17:06:42 -0300

    pong

--- pingpong.js
+++ pingpong.js
@@ -3,6 +3,10 @@ let xBolinha = 300;
 let yBolinha = 200;
 let tamBolinha = 25;
 
+//config velocidade bolinha
+let xvelocidadeBolinha = 6;
+let yvelocidadeBolinha = 6;
+
 function setup(){
     //aqui vou criar minha "mesa"
     createCanvas(600,400);
@@ -11,7 +15,7 @@ function setup(){
 function draw(){
     //aqui vou por a cor da "mesa"
     //A cor da mesa é em rgb
-    background(128,128,0); 
+    background(72,61,139); 
     //chamando a função
     criaBolinha(xBolinha, yBolinha, tamBolinha);
 }
@@ -19,4 +23,9 @@ function draw(){
 //função bolinha
 function criaBolinha(xBolinha, yBolinha,tamBolinha){
     circle (xBolinha,yBolinha,tamBolinha);
+}
+
+//função move bolinha
+function moveBolinha(){}
+    xBolinha =
 }
